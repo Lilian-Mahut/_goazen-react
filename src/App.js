de3e9951521aadb3151928b88cf4a7dc29bb9016
Lilian Mahut <lilian.mahut@pm.me>
2024-02-17 10:46:55 +0100

    installation complète du multilang EN FR ES

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 import Layout from './components/layout/Layout';
+import { useTranslation, Trans } from 'react-i18next';
 
 export default function App() {
-  return <Layout />;
+  const { t } = useTranslation();
+  return (
+    <>
+      <Trans i18nKey="description.part1">
+        Edit <code>src/App.js</code> and save to reload.
+      </Trans>
+      <p>{t('description.part2')}</p>
+      <Layout />
+    </>
+  );
 }
